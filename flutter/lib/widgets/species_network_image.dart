ce49defb45e4800b8dd4482f91a94f5c9ed184eb
Calv-C02 <calvin.chua@live.com>
2026-04-17 21:17:12 +0800

    Attempt to load all species images

--- flutter/lib/widgets/species_network_image.dart
+++ flutter/lib/widgets/species_network_image.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class SpeciesNetworkImage extends StatelessWidget {
@@ -19,20 +20,55 @@ class SpeciesNetworkImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Widget img = CachedNetworkImage(
-      imageUrl: url,
-      fit: fit,
-      height: height,
-      width: width,
-      placeholder: (context, url) => Container(
-        color: Colors.grey.shade200,
-        child: const Center(child: CircularProgressIndicator(strokeWidth: 2)),
-      ),
-      errorWidget: (context, url, error) => Container(
-        color: Colors.grey.shade200,
-        child: Icon(Icons.camera_alt_outlined, color: Colors.grey.shade500, size: 40),
-      ),
-    );
+    Widget img;
+    if (kIsWeb) {
+      // HTML image elements are more tolerant of third-party host policies.
+      img = Image.network(
+        url,
+        fit: fit,
+        height: height,
+        width: width,
+        webHtmlElementStrategy: WebHtmlElementStrategy.prefer,
+        loadingBuilder: (context, child, progress) {
+          if (progress == null) return child;
+          return Container(
+            color: Colors.grey.shade200,
+            child: const Center(
+              child: CircularProgressIndicator(strokeWidth: 2),
+            ),
+          );
+        },
+        errorBuilder: (context, error, stackTrace) => Container(
+          color: Colors.grey.shade200,
+          child: Icon(
+            Icons.camera_alt_outlined,
+            color: Colors.grey.shade500,
+            size: 40,
+          ),
+        ),
+      );
+    } else {
+      img = CachedNetworkImage(
+        imageUrl: url,
+        fit: fit,
+        height: height,
+        width: width,
+        placeholder: (context, url) => Container(
+          color: Colors.grey.shade200,
+          child: const Center(
+            child: CircularProgressIndicator(strokeWidth: 2),
+          ),
+        ),
+        errorWidget: (context, url, error) => Container(
+          color: Colors.grey.shade200,
+          child: Icon(
+            Icons.camera_alt_outlined,
+            color: Colors.grey.shade500,
+            size: 40,
+          ),
+        ),
+      );
+    }
     if (borderRadius != null) {
       img = ClipRRect(borderRadius: borderRadius!, child: img);
     }
